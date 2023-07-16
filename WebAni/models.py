818c79b79f388ddbb45b4ba077b5ee50ac6d700f
Adel5002 <aromat29.11@gmail.com>
2023-07-16 21:26:02 +0300

    Technical commit

--- WebAni/models.py
+++ WebAni/models.py
@@ -1,3 +1,21 @@
+from datetime import date
 from django.db import models
 
-# Create your models here.
+
+class Series(models.Model):
+    poster = models.ImageField(upload_to='poster/')
+    name = models.CharField(max_length=200)
+    japanese_name = models.CharField(max_length=200, blank=True, null=True)
+    premiere = models.DateField(default=date.today)
+    episodes = models.PositiveSmallIntegerField(default=24)
+    STATUS_CHOICES = (
+        ('ongoing', 'Онгоинг'),
+        ('released', 'Вышел'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    # genre = models.ManyToManyField('')
+    # studio = models.ForeignKey()
+    season = models.CharField(blank=True, null=True, max_length=120)
+    slug = models.SlugField(max_length=180, unique=True)
+
+
--- WebAni/views.py
+++ WebAni/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def index(request):
+    api_url = 'https://kodikapi.com/list?token=56a40dfa2b3c22e9f8252a8d6cc78b54&types=anime-serial'
+    response = requests.get(api_url)
+    data = response.json()
+
+
+    titles = [item.get('title') for item in data['results'] if 'title' in item]
+    screenshots = [item.get('screenshots') for item in data['results'] if 'screenshots' in item]
+
+    data_list = [{'title': title, 'screenshot': screenshot} for title, screenshot in zip(titles, screenshots)]
+
+
+    context = {
+        'data': data_list,
+    }
+
+
+    return render(request, 'index.html', context)

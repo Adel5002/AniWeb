fe9e6c82994e544fa8b27b688e38dae60f79b5cd
Adel5002 <aromat29.11@gmail.com>
2023-07-27 20:49:44 +0300

    Mini patch

--- WebAni/urls.py
+++ WebAni/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import index, search
+from .views import AnimeListView, SearchView
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('search/', search, name='search')
+    path('', AnimeListView.as_view(), name='home'),
+    path('search/', SearchView.as_view(), name='search')
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WebAni/views.py
+++ WebAni/views.py
@@ -1,37 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 import requests
 
-def index(request):
-    api_url = 'https://kodikapi.com/list?token=56a40dfa2b3c22e9f8252a8d6cc78b54&limit=100&types=anime&with_material_data=true'
-    response = requests.get(api_url)
-    data = response.json()
 
-    data_list = []
+class AnimeListView(ListView):
+    model = None
+    template_name = 'index.html'
+    context_object_name = 'data'
 
-    unique_titles = set()
+    def get_queryset(self):
+        api_url = 'https://kodikapi.com/list?token=56a40dfa2b3c22e9f8252a8d6cc78b54&limit=100&types=anime&with_material_data=true'
+        response = requests.get(api_url)
+        data = response.json()
 
-    for item in data['results']:
-        title = item.get('title')
-        poster_url = item['material_data']['poster_url']
-        description = item['material_data'].get('description')
-        genres = item['material_data'].get('genres')
-        year = item.get('year')
-        title_orig = item.get('title_orig')
-        # Проверяем, не было ли уже такого заголовка в списке
-        if title not in unique_titles:
-            data_list.append({'title': title, 'poster_url': poster_url, 'description': description, 'year': year, 'title_orig': title_orig, 'genres': genres})
-            unique_titles.add(title)  # Добавляем заголовок во множество
+        data_list = []
 
-    context = {
-        'data': data_list,
-    }
+        unique_titles = set()
 
-    return render(request, 'index.html', context)
+        for item in data['results']:
+            title = item.get('title')
+            poster_url = item['material_data']['poster_url']
+            description = item['material_data'].get('description')
+            genres = item['material_data'].get('genres')
+            year = item.get('year')
+            title_orig = item.get('title_orig')
+            # Проверяем, не было ли уже такого заголовка в списке
+            if title not in unique_titles:
+                data_list.append({'title': title, 'poster_url': poster_url, 'description': description, 'year': year,
+                                  'title_orig': title_orig, 'genres': genres})
+                unique_titles.add(title)  # Добавляем заголовок во множество
+        return data_list
 
 
-def search(request):
-    if 'search_query' in request.GET:
-        search_query = request.GET['search_query']
+class SearchView(ListView):
+    model = None
+    template_name = 'index.html'
+    context_object_name = 'data'
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('search_query')
+        search_query = self.request.GET['search_query']
         api_url = f'https://kodikapi.com/search?token=56a40dfa2b3c22e9f8252a8d6cc78b54&types=anime&with_material_data=true&title={search_query}'
         response = requests.get(api_url)
         data = response.json()
@@ -47,13 +55,8 @@ def search(request):
             year = item.get('year')
             title_orig = item.get('title_orig')
             genres = item['material_data'].get('genres')
-            # Проверяем, не было ли уже такого заголовка в списке
+                    # Проверяем, не было ли уже такого заголовка в списке
             if title not in unique_titles:
                 data_list.append({'title': title, 'poster_url': poster_url, 'description': description, 'year': year, 'title_orig': title_orig, 'genres': genres})
                 unique_titles.add(title)  # Добавляем заголовок во множество
-        context = {
-            'data': data_list,
-        }
-
-        return render(request, 'index.html', context)
-    return render(request, 'index.html')
+        return data_list
